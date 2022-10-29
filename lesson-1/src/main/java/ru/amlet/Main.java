4b1089501a85acf03e000561defb9a3cff5eb73f
20187158 <avyachmikhaylov@sberbank.ru>
2022-10-29 14:38:38 +0300

    lesson-1: NetworkInterface

---
+++ lesson-1/src/main/java/ru/amlet/Main.java
@@ -0,0 +1,9 @@
+package ru.amlet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LessonExecutor lessonExecutor = new LessonExecutor();
+        lessonExecutor.execute();
+    }
+}
